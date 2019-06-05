4cb150aad52c80993796d0204a20e4369bb1fe1c
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-06-05 19:53:13 +0200

    Correction des textes

---
+++ script.js
@@ -0,0 +1,15 @@
+function correct ()
+{
+    var text = document.getElementById('text-input').value;
+
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(event) {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            document.getElementById('text-output').innerText = this.responseText;
+        }
+    };
+
+    request.open('GET', 'correct.php?text='+encodeURI(text), true);
+    request.send(null);
+}
